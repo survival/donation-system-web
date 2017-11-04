90ac558afca98b54e7073c4a207259d4c29cd807
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-11-04 00:14:01 +0000

    Add a wrapper for the Stripe JS library
    This wrapper aims to isolate the application from the Stripe
    library. The implementation follows the documentation for
    the custom integration in the Detailed Checkout Guide page:
    
        https://stripe.com/docs/checkout#integration-custom
    
    Before opening the Checkout pop up, the amount is checked.
    If the validation passes, the pop up launches. If not, it
    defaults to the browser native validation, which won't
    allow you to submit the form until the field is filled,
    and in modern browsers, until the field is filled and is
    a number.

---
+++ public/javascripts/spec/stripe_wrapper_spec.js
@@ -0,0 +1,128 @@
+'use strict';
+
+describe('StripeWrapper', function() {
+  describe('#setup', function() {
+    it('sets the UI to use', function() {
+      StripeWrapper.setup(UI, {}, {});
+      expect(StripeWrapper.ui).toBe(UI);
+    });
+
+    it('initializes Checkout with relevant options', function() {
+      var config = {
+        key: 'foo',
+        image: 'bar',
+        locale: 'es',
+        token: function() {}
+      };
+
+      spyOn(StripeCheckout, 'configure');
+      StripeWrapper.setup(UI, config, {});
+      expect(StripeCheckout.configure).toHaveBeenCalledWith(config);
+    });
+
+    it('sets the params for the Checkout pop up', function() {
+      var params = { foo: 'irrelevant' };
+      StripeWrapper.setup(UI, {}, params);
+      expect(StripeWrapper.params).toBe(params);
+    });
+  });
+
+  describe('#openCheckout', function() {
+    var validAmountUIFake, invalidAmountUIFake, clickEvent;
+
+    beforeAll(function() {
+      validAmountUIFake = {
+        amount: function() { return '20'; },
+        isValidAmount: function() { return true; }
+      };
+      invalidAmountUIFake = {
+        amount: function() { return 'foo'; },
+        isValidAmount: function() { return false; }
+      };
+      clickEvent = createClickEvent();
+    });
+
+    it('does not open Checkout pop up if amount is invalid', function() {
+      StripeWrapper.setup(invalidAmountUIFake, {}, {});
+      spyOn(StripeWrapper.handler, 'open').and.callThrough();
+
+      StripeWrapper.openCheckout(clickEvent);
+
+      expect(StripeWrapper.handler.open).not.toHaveBeenCalled();
+    });
+
+    it('opens Checkout pop up if amount is valid', function() {
+      StripeWrapper.setup(validAmountUIFake, {}, {});
+      StripeWrapper.handler.open = function() {};
+      spyOn(StripeWrapper.handler, 'open').and.callThrough();
+
+      StripeWrapper.openCheckout(clickEvent);
+
+      expect(StripeWrapper.handler.open).toHaveBeenCalled();
+    });
+
+    it('passes params to Checkout pop up', function() {
+      var params, paramsSent;
+      params = {
+        name: 'Name',
+        description: 'description',
+        currency: 'currency',
+        billingAddress: true,
+        zipCode: true
+      };
+      paramsSent = params;
+      paramsSent.amount = 2000;
+
+      StripeWrapper.setup(validAmountUIFake, {}, params);
+      StripeWrapper.handler.open = function() {};
+      spyOn(StripeWrapper.handler, 'open').and.callThrough();
+
+      StripeWrapper.openCheckout(clickEvent);
+
+      expect(StripeWrapper.handler.open).toHaveBeenCalledWith(paramsSent);
+    });
+
+    it('prevents form submission', function() {
+      StripeWrapper.setup(validAmountUIFake, {}, {});
+      StripeWrapper.handler.open = function() {};
+      spyOn(clickEvent, 'preventDefault');
+
+      StripeWrapper.openCheckout(clickEvent);
+
+      expect(clickEvent.preventDefault).toHaveBeenCalled();
+    });
+  });
+
+  describe('#registerListeners', function() {
+    var ui;
+
+    beforeAll(function() {
+      ui = {
+        onButtonClick: function() {},
+        onHistoryChange: function() {}
+      };
+    });
+
+    it('registers a click listener', function() {
+      spyOn(ui, 'onButtonClick');
+      StripeWrapper.setup(ui, {}, {});
+      StripeWrapper.registerListeners();
+      expect(ui.onButtonClick).toHaveBeenCalledWith(StripeWrapper.openCheckout);
+    });
+
+    it('registers a history change listener', function() {
+      spyOn(ui, 'onHistoryChange');
+      StripeWrapper.setup(ui, {}, {});
+      StripeWrapper.registerListeners();
+      expect(ui.onHistoryChange).toHaveBeenCalledWith(StripeWrapper.handler.close);
+    });
+  });
+
+  function createClickEvent() {
+    return new MouseEvent('click', {
+      'view': window,
+      'bubbles': true,
+      'cancelable': true
+    });
+  }
+});
---
+++ public/javascripts/src/stripe_wrapper.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var StripeWrapper = StripeWrapper || {};
+
+StripeWrapper.setup = function(ui, config, params) {
+  StripeWrapper.ui = ui;
+  StripeWrapper.handler = StripeCheckout.configure({
+    key: config.key,
+    image: config.image,
+    locale: config.locale,
+    token: config.token
+  });
+  StripeWrapper.params = params;
+};
+
+StripeWrapper.openCheckout = function(event) {
+  var amount, params;
+  amount = StripeWrapper.ui.amount();
+  params = StripeWrapper.params;
+
+  if (StripeWrapper.ui.isValidAmount(amount)) {
+    StripeWrapper.handler.open({
+      name: params.name,
+      description: params.description,
+      amount: amount * 100,
+      currency: params.currency,
+      billingAddress: params.billingAddress,
+      zipCode: params.billingAddress
+    });
+    event.preventDefault();
+  }
+};
+
+StripeWrapper.registerListeners = function() {
+  StripeWrapper.ui.onButtonClick(StripeWrapper.openCheckout);
+  StripeWrapper.ui.onHistoryChange(StripeWrapper.handler.close);
+};
