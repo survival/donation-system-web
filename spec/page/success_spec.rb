10b392d8f71f69cb0693001cb52aa56d04437bf5
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-11-13 17:38:56 +0000

    Add a presenter for the success view
    This presenter separates the content from the logic behind it and
    allows to keep logic out of the views. Also, separating the strings
    from the view may help in the future with translations, although
    now it is too early to think about that, so for the time being this
    is done just for separation of concerns.

---
+++ spec/page/success_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'spec_helper.rb'
+require_relative '../../lib/page/success'
+
+module Page
+  RSpec.describe Success do
+    let(:page) { described_class.new('https://some.baseurl') }
+
+    it 'has a title' do
+      expect(page.title).not_to be_nil
+    end
+
+    it 'has an image' do
+      expect(page.image_url).to include('https://some.baseurl')
+      expect(page.ui(:image_width)).not_to be_nil
+      expect(page.ui(:image_height)).not_to be_nil
+      expect(page.ui(:image_description)).not_to be_nil
+    end
+
+    it 'has a body' do
+      expect(page.ui(:body)).not_to be_nil
+    end
+
+    it 'has a social share section' do
+      expect(page.ui(:share_this_title)).not_to be_nil
+    end
+  end
+end
