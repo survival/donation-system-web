eb9f2dc3210a47c9e43f5512505ff491873f0a03
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-11-15 21:30:12 +0000

    Plug the presenter for the success page in the routes
    For the time being I am using our current CDN at Amazon, since
    we haven't decided yet if we try a new one or stick with Amazon.
    
    I also introduced Share This for the sharing buttons, since I
    know of them for a long time now and they have support for
    wassap share, which the Spanish office wanted. Share This also
    does some analytics on the clicks that could be useful for
    campaigners or anybody looking at that kind of stats.

--- lib/donation_system_webapp.rb
+++ lib/donation_system_webapp.rb
@@ -2,11 +2,13 @@
 
 require 'sinatra'
 require_relative 'donations'
+require_relative 'page/success'
 
 class DonationSystemWebapp < Sinatra::Base
   set :views, "#{settings.root}/../views"
   set :public_folder, "#{settings.root}/../public"
   set :stripe_public_key, ENV['STRIPE_PUBLIC_KEY']
+  set :assets, 'https://s3.amazonaws.com/assets-production.survivalinternational.org'
 
   get '/' do
     erb :home
@@ -20,6 +22,7 @@ class DonationSystemWebapp < Sinatra::Base
   end
 
   post '/donations' do
+    @page = Page::Success.new(settings.assets)
     erb :success
   end
 end
