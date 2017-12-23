feb5ae51d48aae62fd6eced8713730445d74dacc
octopusinvitro <octopusinvitro@users.noreply.github.com>
2017-12-23 04:47:23 +0000

    Add a class to upload timestamped files to Amazon S3
    This uses the AWS SDK gem to upload CSS and JS to the staging
    and production buckets.

--- lib/error_messages.rb
+++ lib/error_messages.rb
@@ -6,7 +6,16 @@ module ErrorMessages
     deleting_error: 'Deleting file failed for'
   }.freeze
 
+  AMAZON_FILE_UPLOADER_ERRORS = {
+    missing_s3_credentials: 'Missing AWS S3 credentials, bucket not created',
+    missing_bucket: 'Missing AWS S3 bucket or it was not created',
+    missing_local_file: 'Missing local file',
+    multipart_upload_error: 'Multipart AWS S3 upload error',
+    generic_error: 'Error:'
+  }.freeze
+
   ERROR_MESSAGES = FILE_HELPER_ERRORS
+                   .merge(AMAZON_FILE_UPLOADER_ERRORS)
 
   def join(*args)
     args.join(' ')
